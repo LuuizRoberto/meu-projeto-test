a39adc748cc118d0e29b97ccf65af98ada8330a9
Luiz Reis <luiz.reis@avenue.us>
2026-03-31 17:58:10 -0300

    Gerando um conflieto

--- mensagem.py
+++ mensagem.py
@@ -1,4 +1,4 @@
-mensagem = 'Hello World! Utilizando o Git no VsCode'
+mensagem = 'Hello World! Gerando um conflito'
 print('mensagem')
 print("Nova feature adicionada!")
 print('Fazendo o merge com a branch main')
